8f6ac86a6078af315f8ba0da93cf6c727bacfc59
Rashu <shardarashu@gmail.com>
2025-09-09 11:14:08 -0400

    Added and implemented general Multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,19 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int count = multiples(10, 3, 5);
+        System.out.println(count);
+
+    }
+
+    static int multiples(int n, int a, int b) {
         int multipleCount = 0;
 
-        for (int i = 1; i <=1000; i++){
-            if (i % 3 == 0 || i % 5 == 0) {
+        for (int i = 1; i < n; i++){
+            if (i % a == 0 || i % b == 0) {
                 multipleCount++;
             }
         }
-        System.out.println("The number of multiple of 3 or 5 under 1000 is " + multipleCount);
-
+        return multipleCount;
     }
 }
